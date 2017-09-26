7a917825c6c75441ebc3b6551f46ec8cec7dd970
Col-E <mcoley192@icloud.com>
2017-09-25 21:38:27 -0400

    Update docs

--- src/me/coley/recaf/ui/component/internalframe/TryCatchBox.java
+++ src/me/coley/recaf/ui/component/internalframe/TryCatchBox.java
@@ -10,7 +10,6 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
