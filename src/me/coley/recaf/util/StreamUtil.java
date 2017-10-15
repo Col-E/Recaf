c807945c8b0b92c7d156e45ccb8c4eac93f32eb7
Col-E <mcoley192@icloud.com>
2017-10-15 06:49:34 -0400

    Removed unused class, moved single-reference to inner of user

--- src/me/coley/recaf/util/StreamUtil.java
+++ src/me/coley/recaf/util/StreamUtil.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,22 +13,13 @@ public class StreamUtil {
 	private final static int BUFF_SIZE = (int) Math.pow(128, 2);
 
 	/**
-	 * Creates a DataInputStream from byte[].
-	 *
-	 * @param data byte[] to convert to DataInputStream.
-	 * @return The data input stream.
-	 */
-	public static IndexableDataStream fromBytes(byte[] data) {
-		return new IndexableDataStream(data);
-	}
-
-	/**
 	 * Reads the bytes from the inputstream into a byte array.
 	 *
-	 * @param is InputStream to read from.
+	 * @param is
+	 *            InputStream to read from.
 	 * @return byt earray representation of the input stream.
-	 * @throws IOException Thrown if the given input stream cannot be read
-	 * from.
+	 * @throws IOException
+	 *             Thrown if the given input stream cannot be read from.
 	 */
 	public static byte[] fromStream(InputStream is) throws IOException {
 		try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
@@ -42,22 +34,58 @@ public class StreamUtil {
 	}
 
 	/**
-	 * Sorts the stream of java names.
-	 *
-	 * @param stream Stream of names.
-	 * @return The stream, sorted.
-	 */
-	public static Stream<String> sortJavaNames(Stream<String> stream) {
-		return stream.sorted(new JavaNameSorter());
-	}
-
-	/**
 	 * Creates a list of sorted java names from a given collection.
 	 *
-	 * @param names Collection of names.
+	 * @param names
+	 *            Collection of names.
 	 * @return The collection, sorted and converted to a list.
 	 */
 	public static List<String> listOfSortedJavaNames(Collection<String> names) {
 		return sortJavaNames(names.stream()).collect(Collectors.toList());
+	}
+
+	/**
+	 * Sorts the stream of java names.
+	 *
+	 * @param stream
+	 *            Stream of names.
+	 * @return The stream, sorted.
+	 */
+	private static Stream<String> sortJavaNames(Stream<String> stream) {
+		return stream.sorted(new JavaNameSorter());
+	}
+
+	/**
+	 * Comparator for sorting java names. Ensures packages are given higher
+	 * priority than class file names.
+	 * 
+	 * @author Matt
+	 */
+	private static class JavaNameSorter implements Comparator<String> {
+		@Override
+		public int compare(String name1, String name2) {
+			// Split name into sections (by package)
+			String[] name1Sections = name1.split("/");
+			String[] name2Sections = name2.split("/");
+			int name1Length = name1Sections.length;
+			int name2Length = name2Sections.length;
+			int max = Math.min(name1Length, name2Length);
+			// Compare packages
+			for (int i = 0; i < max; i++) {
+				// Ensure packages appear first in sorted order
+				if (i == max - 1 && name1Length != name2Length) {
+					return (name1Length > name2Length) ? -1 : 1;
+				}
+				// Compare section values
+				String p1 = name1Sections[i];
+				String p2 = name2Sections[i];
+				int cmp = p1.compareTo(p2);
+				if (cmp != 0) {
+					return cmp;
+				}
+			}
+			// All sections are equal.
+			return 0;
+		}
 	}
 }
