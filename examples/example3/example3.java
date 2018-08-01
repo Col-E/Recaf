18166b2b93504b1bbce94956d3c7575e4e9f9bf4
vah_13 <vvvaagn@gmail.com>
2018-08-01 10:56:32 +0200

    Create example3.java

---
+++ examples/example3/example3.java
@@ -0,0 +1,14 @@
+package test3;
+public class main {
+	public static void main(String[] args) {
+		int a = 0;
+		if (a==0)
+		{
+			System.out.println("a==0");
+		}
+		else
+		{
+			System.out.println("Jar file patched, a!=0");
+		}
+	}
+}
