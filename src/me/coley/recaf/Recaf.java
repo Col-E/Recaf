f64eb6ded93108a2ec3760633ead790c5457ecdf
Col-E <mcoley2@gmu.edu>
2019-04-09 01:33:56 -0400

    Update version

--- src/me/coley/recaf/Recaf.java
+++ src/me/coley/recaf/Recaf.java
@@ -5,7 +5,7 @@ import me.coley.recaf.bytecode.analysis.Hierarchy;
 import me.coley.recaf.ui.FxWindow;
 
 public class Recaf {
-	public static final String VERSION = "1.11.1";
+	public static final String VERSION = "1.12.0";
 	public static String[] args;
 
 	public static void main(String[] args) {
