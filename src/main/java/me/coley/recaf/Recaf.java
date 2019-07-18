10abfd0a0840617607cbcdefaf2de9bd74c03804
Col-E <mcoley2@gmu.edu>
2019-07-18 00:17:08 -0400

    Add Workspace field + getter/setter/listeners

--- src/main/java/me/coley/recaf/Recaf.java
+++ src/main/java/me/coley/recaf/Recaf.java
@@ -1,5 +1,6 @@
 package me.coley.recaf;
 
+import me.coley.recaf.workspace.Workspace;
 import org.pmw.tinylog.Configurator;
 import org.pmw.tinylog.Logger;
 import org.pmw.tinylog.writers.FileWriter;
@@ -10,6 +11,8 @@ import picocli.CommandLine;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * Entry point &amp; version constant.
@@ -21,12 +24,45 @@ public class Recaf {
 	 * Recaf version.
 	 */
 	public static final String VERSION = "2.0.0";
+	/**
+	 * Current workspace.
+	 */
+	private static Workspace currentWorkspace;
+	/**
+	 * Listeners to call when the {@link #currentWorkspace current workspace} is changed.
+	 */
+	private static Set<Consumer<Workspace>> workspaceSetListeners = new HashSet<>();
 
+	/**
+	 * Start recaf.
+	 * @param args Optional args.
+	 */
 	public static void main(String[] args) {
 		setupLogging();
 		Logger.info("Starting Recaf-{}...", VERSION);
 		// Invoke
 		new CommandLine(new Initializer()).execute(args);
+	}
+
+	/**
+	 * @param currentWorkspace Sets the current workspace.
+	 */
+	public static void setCurrentWorkspace(Workspace currentWorkspace) {
+		Recaf.currentWorkspace = currentWorkspace;
+	}
+
+	/**
+	 * @return Current workspace.
+	 */
+	public static Workspace getCurrentWorkspace() {
+		return currentWorkspace;
+	}
+
+	/**
+	 * @return Set of listeners to call when the {@link #currentWorkspace current workspace} is changed.
+	 */
+	public static Set<Consumer<Workspace>> getWorkspaceSetListeners() {
+		return workspaceSetListeners;
 	}
 
 	public static void setupLogging() {
