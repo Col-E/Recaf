2760da1508047096c1ae70bad58cbb6966749e11
andylizi <andylizi666@gmail.com>
2019-08-11 09:37:34 +0800

    Remove the redundant copying in overlapping results calculation
    Mentioned in #164

--- src/main/java/me/coley/recaf/search/SearchCollector.java
+++ src/main/java/me/coley/recaf/search/SearchCollector.java
@@ -2,12 +2,14 @@ package me.coley.recaf.search;
 
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.Multimaps;
 import me.coley.recaf.util.ClassUtil;
 import me.coley.recaf.workspace.Workspace;
 import org.objectweb.asm.*;
 import org.objectweb.asm.tree.*;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.stream.Stream;
 
@@ -17,23 +19,20 @@ import static org.objectweb.asm.ClassReader.*;
 /**
  * Search result collector.
  *
- * <br><br>
- *
- * <b>Search API TODO List:</b>
- * <ul>
- * <li>Member references in more obscure cases</li>
- * <li>Method inheritance (child of given)</li>
- * <li>Instruction text match (depends on text disassembler being finished)</li>
- * <li>Smart optimization, skip certain visitor-api calls if we know our queries won't need to
- * look there</li>
- * </ul>
- *
  * @author Matt
+ */
+/*
+ * TODO with Search API:
+ *  - Member references in more obscure cases
+ *  - Method inheritance (child of given)
+ *  - Instruction text match (depends on text disassembler being finished)
+ *  - Smart optimization, skip certain visitor-api calls if we know our queries won't need to look there
  */
 public class SearchCollector {
 	public static final int ACC_NOT_FOUND = -1;
 	private final ListMultimap<Query, SearchResult> results = MultimapBuilder
 			.linkedHashKeys(2).arrayListValues().build();
+	private final Map<Query, List<SearchResult>> resultMapView = Multimaps.asMap(results);
 	private final Workspace workspace;
 	private final Collection<Query> queries;
 
@@ -59,44 +58,29 @@ public class SearchCollector {
 
 	/**
 	 * @return Flattened list of the {@link #getResultsMap() result map} containing entries shared
-	 * among multiple queries that share the same common contexts.
+	 * among multiple queries that are similar.
+	 * @see SearchResult#isContextSimilar(SearchResult)
 	 */
-	@SuppressWarnings("unchecked")
 	public List<SearchResult> getOverlappingResults() {
-		// Get results of multiple queries that have either the same parent context
-		// (or just same context in some cases)
-		// TODO: remove asMap() call?
-		return new ArrayList<>(results.asMap().values().stream()
+		// Get results of multiple queries that are similar
+		return resultMapView.values().stream()
+				// Cast the stream to Collection for compatibility with LinkedHashSet
+				.map((Function<List<?>, Collection<SearchResult>>) Collection.class::cast)
 				.reduce((a, b) -> {
-					// Set so we don't get duplicates
-					Set<SearchResult> ret = new LinkedHashSet<>();
-					for (SearchResult resA : a) {
-						for (SearchResult resB : b) {
-							// Contexts must be of the same type
-							if (!resA.getContext().getClass().equals(resB.getContext().getClass()))
-								continue;
-							Context<?> ctxA = resA.getContext();
-							Context<?> ctxB = resB.getContext();
-							if (ctxA instanceof Context.ClassContext ||
-									ctxA instanceof Context.AnnotationContext ||
-									ctxA instanceof Context.MemberContext) {
-								// For class, annotation, and members the contexts should match
-								if (ctxA.compareTo(ctxB) == 0) {
-									ret.add(resA);
-									ret.add(resB);
-								}
-							}  else if (ctxA instanceof Context.InsnContext) {
-								// For instructions the parent contexts should match
-								if (ctxA.getParent().compareTo(ctxB.getParent()) == 0) {
-									ret.add(resA);
-									ret.add(resB);
-								}
+					Set<SearchResult> overlapping = new LinkedHashSet<>(Math.min(a.size(), b.size()));
+					for (SearchResult resultA : a) {
+						for (SearchResult resultB : b) {
+							if (resultA.isContextSimilar(resultB)) {
+								overlapping.add(resultA);
+								overlapping.add(resultB);
 							}
 						}
 					}
-					// Back to list
-					return new ArrayList<>(ret);
-				}).get());
+					return overlapping;
+				})
+				// Cast the Optional to List for compatibility with Collections.emptyList()
+				.map((Function<Collection<SearchResult>, List<SearchResult>>) ArrayList::new)
+				.orElseGet(Collections::emptyList);
 	}
 
 	/**
