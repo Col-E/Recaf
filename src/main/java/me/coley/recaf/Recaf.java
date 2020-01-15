cc15edb9e899dda0d50c028fae7fb50437b7e846
Col-E <mcoley2@gmu.edu>
2020-01-15 14:44:19 -0500

    Update versions

--- src/main/java/me/coley/recaf/Recaf.java
+++ src/main/java/me/coley/recaf/Recaf.java
@@ -8,7 +8,7 @@ public class Recaf {
 	/**
 	 * Recaf version.
 	 */
-	public static final String VERSION = "1.15.8";
+	public static final String VERSION = "1.15.9";
 	/**
 	 * Initial launch arguments.
 	 */
