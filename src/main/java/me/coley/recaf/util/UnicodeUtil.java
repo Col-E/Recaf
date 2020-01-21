2b3559ab308708dbcabec2048455a41d2b8a2609
xDark <19853368+xxDark@users.noreply.github.com>
2020-01-21 20:31:34 +0300

    Drop regex

--- src/main/java/me/coley/recaf/util/UnicodeUtil.java
+++ src/main/java/me/coley/recaf/util/UnicodeUtil.java
@@ -1,17 +1,11 @@
 package me.coley.recaf.util;
 
-import com.github.javaparser.utils.StringEscapeUtils;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Utils to work with Unicode text.
  *
  * @author xxDark
  */
 public final class UnicodeUtil {
-	private static final Pattern PATTERN_CONTROL_CODE = Pattern.compile("^(?!\\u00A7[0-9A-FK-OR])[^\\x00-\\x7F]+");
 
 	private UnicodeUtil() { }
 
@@ -22,12 +16,51 @@ public final class UnicodeUtil {
 	 * @return string with escaped characters.
 	 */
 	public static String unescape(String input) {
-		StringBuffer buffer = new StringBuffer(input.length());
-		Matcher matcher = PATTERN_CONTROL_CODE.matcher(input);
-		while (matcher.find()) {
-			matcher.appendReplacement(buffer, StringEscapeUtils.unescapeJava(matcher.group(1)));
+		int len = input.length();
+		int cursor = 0;
+		StringBuilder builder = new StringBuilder(len);
+		while (cursor < len) {
+			int consumed = escape(input, cursor, builder);
+			if (consumed == 0) {
+				char c1 = input.charAt(cursor++);
+				builder.append(c1);
+				if (Character.isHighSurrogate(c1) && cursor < len) {
+					char c2 = input.charAt(cursor);
+					if (Character.isLowSurrogate(c2)) {
+						builder.append(c2);
+						cursor += 1;
+					}
+				}
+			} else {
+				for (int pt = 0; pt < consumed; ++pt) {
+					cursor += Character.charCount(Character.codePointAt(input, cursor));
+				}
+			}
 		}
-		matcher.appendTail(buffer);
-		return buffer.toString();
+		return builder.toString();
+	}
+
+	private static int escape(String input, int cursor, StringBuilder builder) {
+		if (input.charAt(cursor) != '\\') return 0;
+		if (cursor + 1 >= input.length()) return 0;
+		if (input.charAt(cursor + 1) != 'u') return 0;
+		int i;
+		for (i = 2; cursor + i < input.length() && input.charAt(cursor + i) == 'u'; i++) ;
+
+		if (cursor + i < input.length() && input.charAt(cursor + i) == '+') {
+			i += 1;
+		}
+
+		if (cursor + i + 4 <= input.length()) {
+			String unicode = input.substring(cursor + i, cursor + i + 4);
+			try {
+				int value = Integer.parseInt(unicode, 16);
+				builder.append((char) value);
+			} catch (NumberFormatException ignored) {
+				return 0;
+			}
+			return i + 4;
+		}
+		return 0;
 	}
 }
