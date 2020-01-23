84fa540b5b19264c81918b6fdcaf7f981bdab449
xDark <19853368+xxDark@users.noreply.github.com>
2020-01-23 19:40:33 +0300

    Use popInteger

--- src/main/java/me/xdark/recaf/jvm/instructions/InstructionHandlerIntToChar.java
+++ src/main/java/me/xdark/recaf/jvm/instructions/InstructionHandlerIntToChar.java
@@ -8,10 +8,7 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 public final class InstructionHandlerIntToChar implements InstructionHandler<AbstractInsnNode> {
 	@Override
 	public void process(AbstractInsnNode instruction, ExecutionContext ctx) throws Throwable {
-		Object v = ctx.pop();
-		if (!(v instanceof Integer)) {
-			throw new InvalidBytecodeException("Attempted to pop integer, but value was: " + v);
-		}
-		ctx.push((char) ((Integer) v).intValue());
+		int v = ctx.popInteger();
+		ctx.push((char) v);
 	}
 }
