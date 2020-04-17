f7f218a16abd14a2b5275e44b8df4b96ef83c384
xDark <19853368+xxDark@users.noreply.github.com>
2020-04-17 10:35:08 +0300

    Set recaf.home a bit earlier

--- src/main/java/me/coley/recaf/Recaf.java
+++ src/main/java/me/coley/recaf/Recaf.java
@@ -84,10 +84,10 @@ public class Recaf {
 
 	private static void init() {
 		if (!initialized) {
+			System.setProperty("recaf.home", getDirectory().normalize().toString());
 			SelfPatcher.patch();
 			// Fix title bar not displaying in GTK systems
 			System.setProperty("jdk.gtk.version", "2");
-			System.setProperty("recaf.home", getDirectory().normalize().toString());
 			// Show version & start
 			info("Recaf-{}", VERSION);
 			initialized = true;
