0e042ab4e2feaa4d94c611f848b1b6e63a81782c
Col-E <mcoley2@gmu.edu>
2020-07-31 04:04:00 -0400

    Update version

--- src/main/java/me/coley/recaf/Recaf.java
+++ src/main/java/me/coley/recaf/Recaf.java
@@ -33,7 +33,7 @@ import static me.coley.recaf.util.Log.*;
  * @author Matt
  */
 public class Recaf {
-	public static final String VERSION = "2.3.7";
+	public static final String VERSION = "2.3.8";
 	public static final String DOC_URL = "https://col-e.github.io/Recaf/documentation.html";
 	private static Controller currentController;
 	private static Workspace currentWorkspace;
