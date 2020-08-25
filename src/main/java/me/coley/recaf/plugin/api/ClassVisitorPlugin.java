26aee8c0ae9cb94b3682d00b69522aec6af8d46b
xDark <19853368+xxDark@users.noreply.github.com>
2020-08-25 13:25:30 +0300

    fix: Add docs to class header

--- src/main/java/me/coley/recaf/plugin/api/ClassVisitorPlugin.java
+++ src/main/java/me/coley/recaf/plugin/api/ClassVisitorPlugin.java
@@ -2,6 +2,11 @@ package me.coley.recaf.plugin.api;
 
 import org.objectweb.asm.ClassVisitor;
 
+/**
+ * Allow plugins to intercept and modify {@link ClassVisitor}.
+ *
+ * @author xxDark
+ */
 public interface ClassVisitorPlugin extends BasePlugin {
     /**
      * Intercepts the given {@link ClassVisitor}.
