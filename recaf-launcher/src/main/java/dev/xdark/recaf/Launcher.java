f5c5bfd9e2341ccbf01589dac77598e5afc2f024
xDark <19853368+xxDark@users.noreply.github.com>
2020-12-29 14:06:53 +0300

    Add logback, more verbose logic, make flow more cleaner

--- recaf-launcher/src/main/java/dev/xdark/recaf/Launcher.java
+++ recaf-launcher/src/main/java/dev/xdark/recaf/Launcher.java
@@ -4,10 +4,10 @@ import com.google.gson.GsonBuilder;
 import dev.dirs.BaseDirectories;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -22,6 +22,8 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Recaf's launcher entrypoint.
@@ -30,11 +32,14 @@ import org.apache.commons.io.IOUtils;
  */
 public final class Launcher {
 
+  private static final Logger logger = LoggerFactory.getLogger("Launcher");
+
   private static final String API_URL = "https://api.github.com/repos/Col-E/Recaf/releases/latest";
   private static final String ISSUES_URL = "https://github.com/Col-E/Recaf/issues";
 
+  private static final OpenOption[] WRITE_OPTIONS = {StandardOpenOption.CREATE,
+      StandardOpenOption.TRUNCATE_EXISTING};
   private static final List<String> UPDATE_FAIL = Collections.singletonList("--updateFailed");
-  private static final PrintStream PS = System.err;
 
   /**
    * Deny public constructions.
@@ -51,16 +56,16 @@ public final class Launcher {
     ReleaseInfo info = null;
     try {
       info = fetchReleaseInfo();
-      log("Successfully fetched release info");
+      logger.info("Successfully fetched release info");
     } catch (IOException ex) {
-      log("Could not fetch release info: ", ex);
+      logger.error("Could not fetch release info: ", ex);
     }
 
     OptionParser parser = new OptionParser();
     OptionSpec<Path> jarOption = parser.accepts("home", "Recaf jar file")
         .withRequiredArg()
         .withValuesConvertedBy(new PathValueConverter())
-        .defaultsTo(Paths.get(BaseDirectories.get().dataDir).resolve("Recaf").resolve("Recaf.jar"));
+        .defaultsTo(getDirectory().resolve("Recaf.jar"));
     OptionSpec<Boolean> autoUpdateOption = parser
         .accepts("autoUpdate", "Should the update be done automatically?")
         .withRequiredArg()
@@ -69,93 +74,100 @@ public final class Launcher {
     parser.allowsUnrecognizedOptions();
     OptionSet options = parser.parse(args);
     Path jarPath = options.valueOf(jarOption);
-    boolean exists = Files.exists(jarPath), valid = exists;
-    log("Target jar exists: %s", exists);
+    logger.info("Target jar: {}", jarPath);
+    boolean exists = Files.exists(jarPath);
+
+    logger.info("Target jar exists: {}", exists);
     if (info == null) {
       // We were unable to fetch latest release info.
       // Attempt to continue.
-      if (valid) {
+      if (exists) {
         attemptLaunch(jarPath, UPDATE_FAIL);
       } else {
-        log("Launcher was unable to fetch release info, cannot continue.");
-        log("If you believe that it is a bug, please open an issue at: ");
-        log(ISSUES_URL);
+        logger.error("Launcher was unable to fetch release info, cannot continue");
+        logger.error("If you believe that it is a bug, please open an issue at: ");
+        logger.error(ISSUES_URL);
         System.exit(1);
       }
     }
     boolean autoUpdate = options.valueOf(autoUpdateOption);
-    log("Automatic updates: %s", autoUpdate);
-    if (valid) {
+    logger.info("Automatic updates: {}", autoUpdate);
+    boolean versionValid = false;
+    if (exists) {
       String version = null;
       try (JarFile jar = new JarFile(jarPath.toFile())) {
         Manifest manifest = jar.getManifest();
         version = manifest.getMainAttributes().getValue("Specification-Version");
-        log("Detected current version: %s", version);
+        logger.info("Detected current version: {}", version);
       } catch (IOException ex) {
-        log("Unable to get Recaf version: ", ex);
+        logger.error("Unable to get Recaf version: ", ex);
       }
       if (version == null) {
-        log("Version detected has failed");
-        log("Ensure that Recaf's jar file is not damaged, or");
-        log("Open an issue if you think that it's an error: ");
-        log(ISSUES_URL);
-        valid = false;
+        logger.error("Version detected has failed");
+        logger.error("Ensure that Recaf's jar file is not damaged, or");
+        logger.error("open an issue if you think that it's an error: ");
+        logger.error(ISSUES_URL);
+        versionValid = false;
       } else {
         try {
-          valid = !isOutdated(version, info.getName());
+          versionValid = !isOutdated(version, info.getName());
         } catch (Exception ex) {
-          log("Could not compare versions: ", ex);
-          valid = false;
+          logger.error("Could not compare versions: ", ex);
+          versionValid = false;
         }
       }
     }
     Asset asset = findReleaseAsset(info.getAssets());
     if (asset == null) {
-      log("Unable to detect release asset");
+      logger.error("Unable to detect release asset");
     }
-    if (valid) {
+    boolean areSizesEqual = false;
+    if (versionValid) {
       if (asset == null) {
-        log("Launcher was unable to detect release asset from GitHub releases");
-        log("Please open an issue at: ");
-        log(ISSUES_URL);
+        logger.error("Launcher was unable to detect release asset from GitHub releases");
+        logger.error("Please open an issue at: ");
+        logger.error(ISSUES_URL);
         attemptLaunch(jarPath, UPDATE_FAIL, 1);
       }
       try {
-        valid = asset.getSize() == Files.size(jarPath);
-        log("Jar size match: %s", valid);
+        areSizesEqual = asset.getSize() == Files.size(jarPath);
+        logger.info("Jar size match: {}", areSizesEqual);
       } catch (IOException ex) {
-        log("Could not verify jar size, will assume that jar is valid: ", ex);
+        logger.error("Could not verify jar size, will assume that jar is valid: ", ex);
       }
     }
-    if (!valid) {
+    if (!areSizesEqual) {
       if (exists) {
         boolean writeable = Files.isWritable(jarPath);
         if (!writeable) {
-          log("Jar is not writeable, check your file system permissions");
+          logger.error("Jar is not writeable, check your file system permissions");
           attemptLaunch(jarPath, UPDATE_FAIL, 1);
         }
       }
       // Download new jar.
-      String $url = asset.getUrl();
-      log("Downloading update from: %s", $url);
+      logger.info("Downloading update from: {}", asset.getUrl());
       byte[] bytes;
       try {
-        URL url = new URL($url);
+        URL url = new URL(asset.getUrl());
         try (InputStream in = url.openStream()) {
           bytes = IOUtils.toByteArray(in);
         }
-        log("Downloaded updated jar, starting");
-        Files
-            .write(jarPath, bytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        int actualSize = bytes.length;
+        int requiredSize = asset.getSize();
+        if (actualSize != requiredSize) {
+          logger.error("Size of the received jar is invalid (expected: {}, got: {})", requiredSize, actualSize);
+        }
+        logger.info("Downloaded updated jar, starting");
+        Files.write(jarPath, bytes, WRITE_OPTIONS);
         launch(jarPath, Collections.emptyList());
       } catch (IOException ex) {
-        log("Unable to update jar file, is path writeable? ", ex);
+        logger.error("Unable to update jar file, is path writeable?", ex);
         if (exists) {
           attemptLaunch(jarPath, UPDATE_FAIL, 1);
         }
       }
     } else {
-      log("Current jar seems to be valid, using it");
+      logger.info("Current jar seems to be valid, using it");
       attemptLaunch(jarPath, Collections.emptyList());
     }
   }
@@ -165,7 +177,7 @@ public final class Launcher {
       launch(jar, extraOptions);
       System.exit(exit);
     } catch (IOException ex) {
-      log("Unable to launch Recaf: ", ex);
+      logger.error("Unable to launch Recaf: ", ex);
       System.exit(1);
     }
   }
@@ -197,21 +209,6 @@ public final class Launcher {
     return info;
   }
 
-  private static void log(String message, Exception ex) {
-    PS.println(message);
-    if (ex != null) {
-      ex.printStackTrace(PS);
-    }
-  }
-
-  private static void log(String message) {
-    log(message, (Exception) null);
-  }
-
-  private static void log(String message, Object... args) {
-    log(String.format(message, args));
-  }
-
   private static boolean isOutdated(String current, String latest) {
     String[] arrSelf = current.split("\\.");
     String[] arrLatest = latest.split("\\.");
@@ -232,20 +229,17 @@ public final class Launcher {
         .orElse(null);
   }
 
-  private static final int VERSION_OFFSET = 44;
-
-  private static int vmVersion() {
-    String property = System.getProperty("java.class.version", "");
-    if (!property.isEmpty()) {
-      return (int) (Float.parseFloat(property) - VERSION_OFFSET);
+  private static Path getDirectory() {
+    Path directory;
+    try {
+      directory = Paths.get(BaseDirectories.get().configDir).resolve("Recaf");
+    } catch (Throwable t) {
+      if (System.getProperty("os.name").toLowerCase().contains("win")) {
+        directory = Paths.get(System.getenv("APPDATA"), "Recaf");
+      } else {
+        throw new IllegalStateException("Failed to initialize Recaf directory");
+      }
     }
-    property = System.getProperty("java.vm.specification.version", "");
-    if (property.contains(".")) {
-      return (int) Float.parseFloat(property.substring(property.indexOf('.') + 1));
-    } else if (!property.isEmpty()) {
-      return Integer.parseInt(property);
-    }
-    log("Fallback vm-version fetch failed, defaulting to 8");
-    return 8;
+    return directory;
   }
 }
