b63aa8cfc1bb822b7701dc647706b11b5476697b
Col-E <mcoley2@gmu.edu>
2021-04-14 20:36:30 -0400

    Simplify build process

--- build.cmd
+++ build.cmd
@@ -2,32 +2,18 @@
 ECHO ===========================
 ECHO Build Modes
 ECHO ------
-ECHO Java 8
-ECHO  1. Full build with tests
-ECHO  2. Fast build with no tests
+ECHO  1. Build with tests (Slow)
+ECHO  2. Build with no tests (Fast)
 ECHO ------
-ECHO Java 11 or higher
-ECHO  3. Full build with tests
-ECHO  4. Fast build with no tests
-ECHO ------
-ECHO  5. Exit
+ECHO  3. Exit
 ECHO ===========================
 SET /P MODE=Mode:
-IF %MODE%==1 GOTO FULL8
-IF %MODE%==2 GOTO FAST8
-IF %MODE%==3 GOTO FULL11
-IF %MODE%==4 GOTO FAST11
-IF %MODE%==5 GOTO END
-:FULL8
+IF %MODE%==1 GOTO FULL
+IF %MODE%==2 GOTO FAST
+:FULL
 call mvnw clean package
 GOTO END
-:FAST8
+:FAST
 call mvnw clean package -Dmaven.test.skip -Dcheckstyle.skip
-GOTO END
-:FULL11
-call mvnw clean package -P java11
-GOTO END
-:FAST11
-call mvnw clean package -Dmaven.test.skip -Dcheckstyle.skip -P java11
 GOTO END
 :END
