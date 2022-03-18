ed0910ab1842bb3b7adc6d1699cda8e1db0c46d6
Col-E <mcoley2@gmu.edu>
2022-03-18 05:08:46 -0400

    Hide bar when tab selection after closure is no longer a class

--- recaf-ui/src/main/java/me/coley/recaf/ui/control/NavigationBar.java
+++ recaf-ui/src/main/java/me/coley/recaf/ui/control/NavigationBar.java
@@ -36,6 +36,170 @@ import java.util.Optional;
  */
 public class NavigationBar extends HBox {
     private static final Logger logger = Logging.get(NavigationBar.class);
+    private CommonClassInfo lastClassInfo;
+    private MemberInfo lastMemberInfo;
+
+    private NavigationBar()  {
+        getStyleClass().add("navbar");
+
+        setAlignment(Pos.CENTER_LEFT);
+        setHeight(30);
+        setFillHeight(true);
+
+        setVisible(false);
+        managedProperty().bind(visibleProperty());
+
+        RecafDockingManager docking = RecafDockingManager.getInstance();
+        docking.addTabSelectionListener((region, tab) -> {
+            if (tab instanceof ClassTab){
+                ClassTab classTab = (ClassTab) tab;
+                CommonClassInfo tabClassInfo = classTab.getClassRepresentation().getCurrentClassInfo();
+                update(tabClassInfo, null);
+            }
+        });
+        docking.addTabClosureListener(tab -> {
+            if (tab instanceof ClassTab) {
+                ClassTab classTab = (ClassTab) tab;
+                CommonClassInfo tabClassInfo = classTab.getClassRepresentation().getCurrentClassInfo();
+                if (lastClassInfo == tabClassInfo) {
+                    clear();
+                }
+            }
+        });
+    }
+
+    /**
+     * Tries to update the NavBar with the method or field the caret is placed on.
+     *
+     * @param area
+     * 		Java area the to search in.
+     */
+    public void tryUpdateNavbar(JavaArea area) {
+        CommonClassInfo targetClass = area.getCurrentClassInfo();
+        if (targetClass == null)
+            return;
+        NavigationBar navigationBar = NavigationBar.getInstance();
+        Optional<ParseHitResult> infoAtPosition =area. declarationAtPosition(area.getCaretPosition());
+        if (infoAtPosition.isPresent()) {
+            ParseHitResult result = infoAtPosition.get();
+            if (result.getInfo() instanceof MethodInfo) {
+                MethodInfo method = (MethodInfo) result.getInfo();
+                CommonClassInfo declarator = getDeclarator(targetClass, method);
+                navigationBar.update(declarator, method);
+            } else if (result.getInfo() instanceof FieldInfo) {
+                FieldInfo field = (FieldInfo) result.getInfo();
+                CommonClassInfo declarator = getDeclarator(targetClass, field);
+                navigationBar.update(declarator, field);
+            } else if (result.getInfo() instanceof CommonClassInfo){
+                // Could either be the target class, or an inner class.
+                CommonClassInfo clazz = (CommonClassInfo) result.getInfo();
+                navigationBar.update(clazz, null);
+            }
+        } else {
+            // Can't find a declaration, just use the class.
+            navigationBar.update(targetClass, null);
+        }
+    }
+
+    /**
+     * Some selections belong to fields/methods of inner classes. In these cases the 'current' class is not
+     * the correct parent of the selection, so we need to look up the {@link CommonClassInfo} of the inner.
+     *
+     * @param fallback Class result to use as fallback if the declarator of the member cannot be found.
+     * @param member Member to find declaring type of.
+     * @return Class that declares the member, or the given fallback.
+     */
+    private CommonClassInfo getDeclarator(CommonClassInfo fallback, MemberInfo member) {
+        Workspace workspace = RecafUI.getController().getWorkspace();
+        if (workspace != null) {
+            String owner = member.getOwner();
+            CommonClassInfo result = workspace.getResources().getClass(owner);
+            if (result == null)
+                result = workspace.getResources().getDexClass(owner);
+            if (result != null)
+                return result;
+        }
+        return fallback;
+    }
+
+    /**
+     * Updates the navbar to show the components of classInfo and (optionally) memberInfo.
+     * @param classInfo
+     *  Class information to update the navbar with.
+     * @param memberInfo
+     *  Member info to show after the class node.
+     */
+    public void update(CommonClassInfo classInfo, MemberInfo memberInfo) {
+        this.lastClassInfo = classInfo;
+        this.lastMemberInfo = memberInfo;
+
+        if (classInfo == null && memberInfo == null) {
+            clear();
+            return;
+        }
+
+        setVisible(true);
+
+
+
+        String[] elements = classInfo.getName().split("/");
+
+        if(elements.length == 0)
+            return;
+
+        getChildren().clear();
+        for(int i = 0; i < elements.length; i++){
+            boolean isLast = i == elements.length - 1;
+
+            String elementText = elements[i];
+
+            // Last element will always be a class
+            if(isLast) {
+                getChildren().add(new MemberNavigationNode(elementText, classInfo));
+            } else {
+                // Normal elements don't need icons
+                getChildren().add(new Label(elementText));
+                getChildren().add(new NavigationSeparator());
+            }
+        }
+
+        if(memberInfo == null)
+            return;
+
+        getChildren().add(new NavigationSeparator());
+
+        Node icon = null;
+        if(memberInfo instanceof MethodInfo) {
+            MethodInfo methodInfo = (MethodInfo)memberInfo;
+            icon = Icons.getMethodIcon(methodInfo);
+        } else if (memberInfo instanceof FieldInfo) {
+            FieldInfo fieldInfo = (FieldInfo)memberInfo;
+            icon = Icons.getFieldIcon(fieldInfo);
+        } else {
+            logger.error("Cannot find icon for unknown type: " + memberInfo.getName());
+        }
+
+        // Let people click on the method/field to bring up the member selection menu
+        getChildren().add(new MemberNavigationNode(memberInfo.getName(), classInfo, icon));
+    }
+
+    /**
+     * Clears all children and makes self invisible.
+     */
+    public void clear() {
+        setVisible(false);
+        getChildren().clear();
+    }
+
+    /**
+     * Gets the navigation bar instance.
+     * Note: There is only going to be one navigation bar for now until the docking system is reworked.
+     */
+    private static final NavigationBar instance = new NavigationBar();
+
+    public static NavigationBar getInstance() {
+        return instance;
+    }
 
     private static class NavigationSeparator extends Canvas {
         public NavigationSeparator() {
@@ -103,151 +267,6 @@ public class NavigationBar extends HBox {
             for(MethodInfo method : classInfo.getMethods())
                 menu.getItems().add(new ActionMenuItem(method.getName(), Icons.getMethodIcon(method), () -> CommonUX.openMember(classInfo, method)));
 
-          }
-    }
-
-    private NavigationBar()  {
-        getStyleClass().add("navbar");
-
-        setAlignment(Pos.CENTER_LEFT);
-        setHeight(30);
-        setFillHeight(true);
-
-        setVisible(false);
-        managedProperty().bind(visibleProperty());
-
-        RecafDockingManager docking = RecafDockingManager.getInstance();
-        docking.addTabSelectionListener((region, tab) -> {
-            if (tab instanceof ClassTab){
-                ClassTab classTab = (ClassTab) tab;
-                CommonClassInfo tabClassInfo = classTab.getClassRepresentation().getCurrentClassInfo();
-                update(tabClassInfo, null);
-            }
-        });
-    }
-
-    /**
-     * Tries to update the NavBar with the method or field the caret is placed on.
-     *
-     * @param area
-     * 		Java area the to search in.
-     */
-    public void tryUpdateNavbar(JavaArea area) {
-        CommonClassInfo targetClass = area.getCurrentClassInfo();
-        if (targetClass == null)
-            return;
-        NavigationBar navigationBar = NavigationBar.getInstance();
-        Optional<ParseHitResult> infoAtPosition =area. declarationAtPosition(area.getCaretPosition());
-        if (infoAtPosition.isPresent()) {
-            ParseHitResult result = infoAtPosition.get();
-            if (result.getInfo() instanceof MethodInfo) {
-                MethodInfo method = (MethodInfo) result.getInfo();
-                CommonClassInfo declarator = getDeclarator(targetClass, method);
-                navigationBar.update(declarator, method);
-            } else if (result.getInfo() instanceof FieldInfo) {
-                FieldInfo field = (FieldInfo) result.getInfo();
-                CommonClassInfo declarator = getDeclarator(targetClass, field);
-                navigationBar.update(declarator, field);
-            } else if (result.getInfo() instanceof CommonClassInfo){
-                // Could either be the target class, or an inner class.
-                CommonClassInfo clazz = (CommonClassInfo) result.getInfo();
-                navigationBar.update(clazz, null);
-            }
-        } else {
-            // Can't find a declaration, just use the class.
-            navigationBar.update(targetClass, null);
         }
-    }
-
-    /**
-     * Some selections belong to fields/methods of inner classes. In these cases the 'current' class is not
-     * the correct parent of the selection, so we need to look up the {@link CommonClassInfo} of the inner.
-     *
-     * @param fallback Class result to use as fallback if the declarator of the member cannot be found.
-     * @param member Member to find declaring type of.
-     * @return Class that declares the member, or the given fallback.
-     */
-    private CommonClassInfo getDeclarator(CommonClassInfo fallback, MemberInfo member) {
-        Workspace workspace = RecafUI.getController().getWorkspace();
-        if (workspace != null) {
-            String owner = member.getOwner();
-            CommonClassInfo result = workspace.getResources().getClass(owner);
-            if (result == null)
-                result = workspace.getResources().getDexClass(owner);
-            if (result != null)
-                return result;
-        }
-        return fallback;
-    }
-
-    /**
-     * Updates the navbar to show the components of classInfo and (optionally) memberInfo.
-     * @param classInfo
-     *  Class information to update the navbar with.
-     * @param memberInfo
-     *  Member info to show after the class node.
-     */
-    public void update(CommonClassInfo classInfo, MemberInfo memberInfo) {
-        setVisible(true);
-        managedProperty().bind(visibleProperty());
-
-        String[] elements = classInfo.getName().split("/");
-
-        if(elements.length == 0)
-            return;
-
-        getChildren().clear();
-        for(int i = 0; i < elements.length; i++){
-            boolean isLast = i == elements.length - 1;
-
-            String elementText = elements[i];
-
-            // Last element will always be a class
-            if(isLast) {
-                getChildren().add(new MemberNavigationNode(elementText, classInfo));
-            } else {
-                // Normal elements don't need icons
-                getChildren().add(new Label(elementText));
-                getChildren().add(new NavigationSeparator());
-            }
-        }
-
-        if(memberInfo == null)
-            return;
-
-        getChildren().add(new NavigationSeparator());
-
-        Node icon = null;
-        if(memberInfo instanceof MethodInfo) {
-            MethodInfo methodInfo = (MethodInfo)memberInfo;
-            icon = Icons.getMethodIcon(methodInfo);
-        } else if (memberInfo instanceof FieldInfo) {
-            FieldInfo fieldInfo = (FieldInfo)memberInfo;
-            icon = Icons.getFieldIcon(fieldInfo);
-        } else {
-            logger.error("Cannot find icon for unknown type: " + memberInfo.getName());
-        }
-
-        // Let people click on the method/field to bring up the member selection menu
-        getChildren().add(new MemberNavigationNode(memberInfo.getName(), classInfo, icon));
-    }
-
-    /**
-     * Clears all children and makes self invisible.
-     */
-    public void clear() {
-        setVisible(false);
-        managedProperty().bind(visibleProperty());
-        getChildren().clear();
-    }
-
-    /**
-     * Gets the navigation bar instance.
-     * Note: There is only going to be one navigation bar for now until the docking system is reworked.
-     */
-    private static final NavigationBar instance = new NavigationBar();
-
-    public static NavigationBar getInstance() {
-        return instance;
     }
 }
