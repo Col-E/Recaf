3e6a12bc57c89e589bf5f15800491968e48dee10
Justus Garbe <gihihoh@gmail.com>
2022-05-14 01:09:12 +0200

    Added onCompleteTokenize for Token list view

--- recaf-assembler/src/main/java/me/coley/recaf/assemble/pipeline/ParserCompletionListener.java
+++ recaf-assembler/src/main/java/me/coley/recaf/assemble/pipeline/ParserCompletionListener.java
@@ -2,11 +2,14 @@ package me.coley.recaf.assemble.pipeline;
 
 import me.coley.recaf.assemble.ast.Unit;
 import me.darknet.assembler.parser.Group;
+import me.darknet.assembler.parser.Token;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface ParserCompletionListener {
+
+    void onCompleteTokenize(Collection<Token> tokens);
 
     /**
      * Called when the parser has completed parsing.
