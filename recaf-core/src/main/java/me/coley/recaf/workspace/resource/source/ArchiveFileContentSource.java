e282a75fafa0c3a2964d44efd952c67a35b9dc2d
xDark <19853368+xxDark@users.noreply.github.com>
2022-05-14 23:24:49 +0300

    Close mapped buffers to prevent file lock

--- recaf-core/src/main/java/me/coley/recaf/workspace/resource/source/ArchiveFileContentSource.java
+++ recaf-core/src/main/java/me/coley/recaf/workspace/resource/source/ArchiveFileContentSource.java
@@ -3,12 +3,15 @@ package me.coley.recaf.workspace.resource.source;
 import me.coley.recaf.io.ByteSource;
 import me.coley.recaf.io.ByteSourceConsumer;
 import me.coley.recaf.io.ByteSourceElement;
+import me.coley.recaf.util.ReflectUtil;
 import software.coley.llzip.ZipArchive;
 import software.coley.llzip.ZipCompressions;
 import software.coley.llzip.ZipIO;
 import software.coley.llzip.part.LocalFileHeader;
+import software.coley.llzip.strategy.JvmZipReaderStrategy;
 import software.coley.llzip.util.ByteData;
 import software.coley.llzip.util.ByteDataUtil;
+import software.coley.llzip.util.FileMapUtil;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -28,17 +31,26 @@ public abstract class ArchiveFileContentSource extends ContainerContentSource<Lo
 
 	@Override
 	protected void consumeEach(ByteSourceConsumer<LocalFileHeader> entryHandler) throws IOException {
-		ZipArchive archive = ZipIO.readJvm(getPath());
-		for (LocalFileHeader fileHeader : archive.getLocalFiles()) {
-			entryHandler.accept(fileHeader, new LocalFileHeaderSource(fileHeader));
+		ByteData data = FileMapUtil.map(getPath());
+		try {
+			ZipArchive archive = ZipIO.read(data, new JvmZipReaderStrategy());
+			for (LocalFileHeader fileHeader : archive.getLocalFiles()) {
+				entryHandler.accept(fileHeader, new LocalFileHeaderSource(fileHeader));
+			}
+		} finally {
+			ReflectUtil.quietInvoke(Object.class, data, "finalize", new Class[0], new Object[0]);
 		}
 	}
 
 	@Override
 	protected Stream<ByteSourceElement<LocalFileHeader>> stream() throws IOException {
-		ZipArchive archive = ZipIO.readJvm(getPath());
+		ByteData data = FileMapUtil.map(getPath());
+		ZipArchive archive = ZipIO.read(data, new JvmZipReaderStrategy());
 		return archive.getLocalFiles().stream()
-				.map(x -> new ByteSourceElement<>(x, new LocalFileHeaderSource(x)));
+				.map(x -> new ByteSourceElement<>(x, new LocalFileHeaderSource(x)))
+				.onClose(() -> {
+					ReflectUtil.quietInvoke(Object.class, data, "finalize", new Class[0], new Object[0]);
+				});
 	}
 
 	@Override
