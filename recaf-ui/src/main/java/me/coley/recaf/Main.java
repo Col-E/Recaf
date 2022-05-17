2746220e92147149c2edd00761d9b8b8537c9a89
Col-E <mcoley2@gmu.edu>
2022-05-16 22:54:37 -0400

    Do not run script on BG thread for non-gui presentation modes

--- recaf-ui/src/main/java/me/coley/recaf/Main.java
+++ recaf-ui/src/main/java/me/coley/recaf/Main.java
@@ -1,6 +1,7 @@
 package me.coley.recaf;
 
 import me.coley.recaf.launch.InitializerParameters;
+import me.coley.recaf.presentation.PresentationType;
 import me.coley.recaf.scripting.ScriptEngine;
 import me.coley.recaf.scripting.ScriptResult;
 import me.coley.recaf.util.Directories;
@@ -35,14 +36,21 @@ public class Main {
 		if (parameters.getScriptPath() != null) {
 			Path scriptPath = parameters.getScriptPath().toPath();
 			if (Files.isRegularFile(scriptPath)) {
-				ThreadUtil.run(() -> {
+				Runnable r = () -> {
 					ScriptResult result = ScriptEngine.execute(scriptPath);
 					if (result.wasSuccess()) {
 						logger.info("Script execute result: {}", result.getResult());
 					} else {
 						logger.error("Script encountered error: ", result.getException());
 					}
-				});
+				};
+				if (parameters.getPresentationType() == PresentationType.GUI) {
+					// Run the script on a delay, giving time to for the GUI to populate
+					ThreadUtil.runDelayed(500, r);
+				} else {
+					// Run the script on the main thread
+					r.run();
+				}
 			} else {
 				logger.error("No script found: {}", scriptPath);
 			}
