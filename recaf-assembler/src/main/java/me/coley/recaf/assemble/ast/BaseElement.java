f6bbc197faba67d1b8d612962ab942761fa3adfc
Justus Garbe <gihihoh@gmail.com>
2022-05-22 17:54:35 +0200

    Implemented binary search for finding elements

--- recaf-assembler/src/main/java/me/coley/recaf/assemble/ast/BaseElement.java
+++ recaf-assembler/src/main/java/me/coley/recaf/assemble/ast/BaseElement.java
@@ -148,11 +148,29 @@ public abstract class BaseElement implements Element {
 		return closestElement;
 
 	}
-
 	public Element getChildAt(int position) {
-		for (Element child : getChildren()) {
-			if (child.getStart() <= position && child.getStop() >= position) {
-				return child;
+		Element[] elements = getChildren().toArray(new Element[0]);
+		if (elements.length == 0) { // base case
+			return null;
+		}
+		if (elements.length == 1) { // base case
+			return elements[0];
+		}
+
+		// perform binary search
+		int start = 0;
+		int end = elements.length - 1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (position < elements[mid].getStart()) {
+				end = mid - 1;
+			}
+			else if (position > elements[mid].getStop()) {
+				start = mid + 1;
+			}
+			else {
+				return elements[mid];
 			}
 		}
 		return null;
