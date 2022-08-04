2792723b1024035beb1366dffbac55848fbd714e
Col-E <mcoley2@gmu.edu>
2022-08-04 18:57:21 -0400

    Support for full class definitions in script engine

--- recaf-ui/src/main/java/me/coley/recaf/scripting/ScriptEngine.java
+++ recaf-ui/src/main/java/me/coley/recaf/scripting/ScriptEngine.java
@@ -18,14 +18,17 @@ import java.nio.file.Path;
 import java.util.*;
 
 /**
- * Base script engine for running scripts.
+ * Engine for running scripts.
  *
- * @author Wolfie / win32kbase
+ * @author Matt Coley
  */
 public class ScriptEngine {
 	private static final Logger logger = Logging.get(ScriptEngine.class);
-	private static final Map<String, GenerateResult> SCRIPT_CLASS_CACHE = new HashMap<>();
+	private static final Map<Integer, GenerateResult> SCRIPT_CLASS_CACHE = new HashMap<>();
 	private static final String SCRIPT_PACKAGE_NAME = "me.coley.recaf.scripting.generated";
+	private static final String PATTERN_PACKAGE = "package ([\\w\\.\\*]+);?";
+	private static final String PATTERN_IMPORT = "import ([\\w\\.\\*]+);?";
+	private static final String PATTERN_CLASS_NAME = "(?<=class)\\s+(\\w+)\\s+(?:implements|extends|\\{)";
 	private static final List<String> DEFAULT_IMPORTS = Arrays.asList(
 			"java.io.*",
 			"java.nio.file.*",
@@ -65,9 +68,16 @@ public class ScriptEngine {
 	}
 
 	private static ScriptResult handleExecute(String script) {
-		logger.info("Executing script");
-		String className = "Script" + Math.abs(script.hashCode());
-		GenerateResult result = SCRIPT_CLASS_CACHE.computeIfAbsent(className, n -> generate(n, script));
+		int hash = script.hashCode();
+		GenerateResult result;
+		if (RegexUtil.matchesAny(PATTERN_CLASS_NAME, script)) {
+			logger.info("Executing script class");
+			result = SCRIPT_CLASS_CACHE.computeIfAbsent(hash, n -> generateStandardClass(script));
+		} else {
+			logger.info("Executing script");
+			String className = "Script" + Math.abs(hash);
+			result = SCRIPT_CLASS_CACHE.computeIfAbsent(hash, n -> generateScriptClass(className, script));
+		}
 		if (result.cls != null) {
 			try {
 				Method run = result.cls.getDeclaredMethod("run");
@@ -84,9 +94,28 @@ public class ScriptEngine {
 		}
 	}
 
-	private static GenerateResult generate(String className, String script) {
+	private static GenerateResult generateStandardClass(String source) {
+		// Extract package name
+		String packageName = SCRIPT_PACKAGE_NAME;
+		Matcher matcher = RegexUtil.getMatcher(PATTERN_PACKAGE, source);
+		if (matcher.find())
+			packageName = matcher.group(1);
+		packageName = packageName.replace('.', '/');
+		// Extract class name
+		String className = null;
+		matcher = RegexUtil.getMatcher(PATTERN_CLASS_NAME, source);
+		if (matcher.find()) {
+			className = packageName + "/" + matcher.group(1);
+		} else {
+			return new GenerateResult(null, List.of(new CompilerDiagnostic(-1, "Could not determine name of class")));
+		}
+		// Compile the class
+		return defineClass(className, source);
+	}
+
+	private static GenerateResult generateScriptClass(String className, String script) {
 		Set<String> imports = new HashSet<>(DEFAULT_IMPORTS);
-		Matcher matcher = RegexUtil.getMatcher("import ([\\w\\.\\*]+);?", script);
+		Matcher matcher = RegexUtil.getMatcher(PATTERN_IMPORT, script);
 		while (matcher.find()) {
 			// Record import statement
 			String importIdentifier = matcher.group(1);
@@ -103,17 +132,12 @@ public class ScriptEngine {
 		code.insert(0, "package " + SCRIPT_PACKAGE_NAME + "; ");
 		className = SCRIPT_PACKAGE_NAME.replace('.', '/') + "/" + className;
 		// Compile the class
-		JavacCompiler compiler = new JavacCompiler();
+		return defineClass(className, code.toString());
+	}
+
+	private static GenerateResult defineClass(String className, String source) {
 		List<CompilerDiagnostic> diagnostics = new ArrayList<>();
-		Map<String, CompileOption<?>> options = compiler.getDefaultOptions();
-		compiler.setCompileListener(diagnostic -> diagnostics.add(
-				new CompilerDiagnostic(
-						(int) diagnostic.getLineNumber() - 1,
-						diagnostic.getMessage(Locale.US))
-		));
-		compiler.setLogging(false);
-		compiler.setDebug(options, JavacCompiler.createDebugValue(true, true, true));
-		CompilerResult result = compiler.compile(className, code.toString(), options);
+		CompilerResult result = compile(className, source, diagnostics);
 		if (result.wasSuccess()) {
 			try {
 				Class<?> cls = DefineUtil.create(result.getValue(), className);
@@ -123,6 +147,19 @@ public class ScriptEngine {
 			}
 		}
 		return new GenerateResult(null, diagnostics);
+	}
+
+	private static CompilerResult compile(String className, String code, List<CompilerDiagnostic> diagnostics) {
+		JavacCompiler compiler = new JavacCompiler();
+		Map<String, CompileOption<?>> options = compiler.getDefaultOptions();
+		compiler.setCompileListener(diagnostic -> diagnostics.add(
+				new CompilerDiagnostic(
+						(int) diagnostic.getLineNumber() - 1,
+						diagnostic.getMessage(Locale.US))
+		));
+		compiler.setLogging(false);
+		compiler.setDebug(options, JavacCompiler.createDebugValue(true, true, true));
+		return compiler.compile(className, code, options);
 	}
 
 	private static class GenerateResult {
