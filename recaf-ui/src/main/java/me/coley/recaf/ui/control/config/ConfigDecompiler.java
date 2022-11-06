332521903d5eb86569943289de7f70380bb1c335
Col-E <mcoley2@gmu.edu>
2022-11-06 11:28:52 -0500

    Always display Fallback decompiler last in combo-box

--- recaf-ui/src/main/java/me/coley/recaf/ui/control/config/ConfigDecompiler.java
+++ recaf-ui/src/main/java/me/coley/recaf/ui/control/config/ConfigDecompiler.java
@@ -1,36 +1,30 @@
 package me.coley.recaf.ui.control.config;
 
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.SingleSelectionModel;
-import me.coley.recaf.RecafUI;
 import me.coley.recaf.config.ConfigContainer;
-import me.coley.recaf.decompile.DecompileManager;
+import me.coley.recaf.ui.control.DecompilerCombo;
 import me.coley.recaf.util.ReflectUtil;
 
 import java.lang.reflect.Field;
 
 /**
  * ComboBox for switching between available decompiler implementations.
+ * Updates the config of preferred decompiler.
  *
  * @author Wolfie / win32kbase
  */
-public class ConfigDecompiler extends ComboBox<String> implements Unlabeled {
-    /**
-     * @param instance
-     * 		Config container.
-     * @param field
-     * 		Config field.
-     */
-    public ConfigDecompiler(ConfigContainer instance, Field field) {
-        DecompileManager decompileManager = RecafUI.getController().getServices().getDecompileManager();
-        decompileManager.getRegisteredImpls().forEach(decompiler -> this.getItems().add(decompiler.getName()));
+public class ConfigDecompiler extends DecompilerCombo implements Unlabeled {
+	/**
+	 * @param instance
+	 * 		Config container.
+	 * @param field
+	 * 		Config field.
+	 */
+	public ConfigDecompiler(ConfigContainer instance, Field field) {
+		// Select the default (currently loaded) decompiler
+		String initialDecompilerName = ReflectUtil.quietGet(instance, field).toString();
+		select(initialDecompilerName);
 
-        // Select the default (currently loaded) decompiler
-        SingleSelectionModel<String> selectionModel = getSelectionModel();
-        selectionModel.select(ReflectUtil.quietGet(instance, field).toString());
-
-        selectionModel.selectedItemProperty().addListener((observable, oldValue, newValue) ->
-                ReflectUtil.quietSet(instance, field, newValue));
-    }
-
+		getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+				ReflectUtil.quietSet(instance, field, newValue));
+	}
 }
