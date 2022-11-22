ce028339864bddfff63214bd8025b7212f16e807
Justus Garbe <gihihoh@gmail.com>
2022-11-22 21:56:48 +0100

    Fix args causing null pointer exception

--- recaf-assembler/src/main/java/me/coley/recaf/assemble/ast/insn/IndyInstruction.java
+++ recaf-assembler/src/main/java/me/coley/recaf/assemble/ast/insn/IndyInstruction.java
@@ -91,9 +91,7 @@ public class IndyInstruction extends AbstractInstruction {
 		sb.append(name).append(' ');
 		sb.append(EscapeUtil.escapeNonValid(desc)).append(' ');
 		sb.append(context.fmtKeyword("handle ")).append(handle).append(' ');
-		if(args.length() > 0) {
-			sb.append(context.fmtKeyword("args ")).append(args).append(" ").append(context.fmtKeyword("end"));
-		}
+		sb.append(context.fmtKeyword("args ")).append(args).append(" ").append(context.fmtKeyword("end"));
 		return sb.toString();
 	}
 
