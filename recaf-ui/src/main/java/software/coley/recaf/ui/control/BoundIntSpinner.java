cb7eaad64a7f202452f3a75083bb62f49da05329
Col-E <mcoley2@gmu.edu>
2023-09-09 18:53:34 -0400

    Create spinner for int properties

---
+++ recaf-ui/src/main/java/software/coley/recaf/ui/control/BoundIntSpinner.java
@@ -0,0 +1,48 @@
+package software.coley.recaf.ui.control;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import javafx.beans.property.IntegerProperty;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+
+/**
+ * Integer spinner with binding to a {@link IntegerProperty}.
+ *
+ * @author Matt Coley
+ */
+public class BoundIntSpinner extends Spinner<Integer> implements Tooltipable {
+	private static final NumberFormat NUMBER_FORMAT = NumberFormat.getIntegerInstance();
+
+	/**
+	 * @param value
+	 * 		property.
+	 */
+	public BoundIntSpinner(@Nonnull IntegerProperty value) {
+		setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(Integer.MIN_VALUE, Integer.MAX_VALUE, value.get()));
+		setEditable(true);
+		setMaxWidth(Double.MAX_VALUE);
+		getEditor().setText(Integer.toString(value.getValue())); // TODO: This does not seem to stick
+		getEditor().setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), 0, BoundIntSpinner::handleChange));
+		value.bind(valueProperty());
+	}
+
+	@Nullable
+	private static TextFormatter.Change handleChange(@Nonnull TextFormatter.Change c) {
+		if (c.isContentChange()) {
+			ParsePosition pos = new ParsePosition(0);
+			// NumberFormat evaluates the beginning of the text.
+			BoundIntSpinner.NUMBER_FORMAT.parse(c.getControlNewText(), pos);
+			if (pos.getIndex() == 0 || pos.getIndex() < c.getControlNewText().length()) {
+				// Reject parsing the complete text failed.
+				return null;
+			}
+		}
+		return c;
+	}
+}
