8fad67a56d60aee49d502f53a5614bf4f4087476
Col-E <mcoley2@gmu.edu>
2023-10-22 16:49:59 -0400

    Add extra JFX validation exit codes for min supported versions

--- recaf-ui/src/main/java/software/coley/recaf/util/JFXValidation.java
+++ recaf-ui/src/main/java/software/coley/recaf/util/JFXValidation.java
@@ -1,6 +1,7 @@
 package software.coley.recaf.util;
 
 import org.slf4j.Logger;
+import regexodus.Matcher;
 import software.coley.recaf.analytics.logging.Logging;
 
 import java.lang.reflect.InaccessibleObjectException;
@@ -12,12 +13,15 @@ import java.lang.reflect.InvocationTargetException;
  * @author Matt Coley
  */
 public class JFXValidation {
+	public static final int MIN_JFX_VERSION = 19;
 	public static final int SUCCESS = 0;
-	public static final int ERR_CLASS_NOT_FOUND = 100;
-	public static final int ERR_NO_SUCH_METHOD = 101;
-	public static final int ERR_INVOKE_TARGET = 102;
-	public static final int ERR_ACCESS_TARGET = 103;
-	public static final int ERR_UNKNOWN = 200;
+	public static final int ERR_UNKNOWN = 100;
+	public static final int ERR_CLASS_NOT_FOUND = 101;
+	public static final int ERR_NO_SUCH_METHOD = 102;
+	public static final int ERR_INVOKE_TARGET = 103;
+	public static final int ERR_ACCESS_TARGET = 104;
+	public static final int ERR_OLD_JFX_VERSION = 105;
+	public static final int ERR_UNKNOWN_JFX_VERSION = 106;
 	private static final Logger logger = Logging.get(JFXValidation.class);
 
 	/**
@@ -28,7 +32,22 @@ public class JFXValidation {
 			String jfxVersionClass = "com.sun.javafx.runtime.VersionInfo";
 			Class<?> versionClass = Class.forName(jfxVersionClass);
 			ReflectUtil.getDeclaredMethod(versionClass, "setupSystemProperties").invoke(null);
-			logger.info("JavaFX initialized: {}", System.getProperty("javafx.version"));
+
+			String versionProperty = System.getProperty("javafx.version");
+			Matcher versionMatcher = RegexUtil.getMatcher("\\d+", versionProperty);
+
+			if (versionMatcher.find()) {
+				int majorVersion = Integer.parseInt(versionMatcher.group());
+				if (majorVersion < MIN_JFX_VERSION) {
+					logger.error("JavaFX version {} is present, but Recaf requires {}+", majorVersion, MIN_JFX_VERSION);
+					return ERR_OLD_JFX_VERSION;
+				}
+			} else {
+				logger.error("JavaFX version {} does not declare a major release version, cannot validate compatibility", versionProperty);
+				return ERR_UNKNOWN_JFX_VERSION;
+			}
+
+			logger.info("JavaFX successfully initialized: {}", versionProperty);
 			return SUCCESS;
 		} catch (ClassNotFoundException ex) {
 			logger.error("JFX validation failed, could not find 'VersionInfo' class", ex);
