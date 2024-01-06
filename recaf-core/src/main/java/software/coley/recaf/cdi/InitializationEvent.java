6468d4ecb61d09c8fcb787966a6c69a544ceb84a
Matt Coley <mcoley@ctic.us>
2024-01-06 12:51:12 -0500

    Improve CDI event handling for startup tasks

---
+++ recaf-core/src/main/java/software/coley/recaf/cdi/InitializationEvent.java
@@ -0,0 +1,10 @@
+package software.coley.recaf.cdi;
+
+/**
+ * Empty type, used to notify CDI consumers observing this type that the application has been launched.
+ *
+ * @author Matt Coley
+ * @see UiInitializationEvent Alternative which waits for the UI to initialize.
+ */
+public class InitializationEvent {
+}
--- recaf-core/src/main/java/software/coley/recaf/cdi/UiInitializationEvent.java
+++ recaf-core/src/main/java/software/coley/recaf/cdi/UiInitializationEvent.java
@@ -4,6 +4,7 @@ package software.coley.recaf.cdi;
  * Empty type, used to notify CDI consumers observing this type that the UI has been populated.
  *
  * @author Matt Coley
+ * @see InitializationEvent Alternative which runs before the UI is initialized.
  */
 public class UiInitializationEvent {
 }
