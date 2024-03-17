7446a917b87fa0e0c63cfb365fcc7509823df0ef
Matt <mcoley2@gmu.edu>
2024-03-17 17:59:45 -0400

    Remove InstructionPathNode static string conversion

--- recaf-core/src/main/java/software/coley/recaf/path/InstructionPathNode.java
+++ recaf-core/src/main/java/software/coley/recaf/path/InstructionPathNode.java
@@ -2,14 +2,10 @@ package software.coley.recaf.path;
 
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.tree.*;
-import org.objectweb.asm.util.Textifier;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import software.coley.recaf.info.member.ClassMember;
 import software.coley.recaf.info.member.MethodMember;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Set;
 
 /**
@@ -46,7 +42,7 @@ public class InstructionPathNode extends AbstractPathNode<ClassMember, AbstractI
 	 * @param index
 	 * 		Index of the instruction within the method code.
 	 *
-	 * @see ClassMemberPathNode#childInsn(AbstractInsnNode,int)
+	 * @see ClassMemberPathNode#childInsn(AbstractInsnNode, int)
 	 */
 	public InstructionPathNode(@Nullable ClassMemberPathNode parent, @Nonnull AbstractInsnNode insn, int index) {
 		super(TYPE_ID, parent, insn);
@@ -54,7 +50,7 @@ public class InstructionPathNode extends AbstractPathNode<ClassMember, AbstractI
 	}
 
 	/**
-	 * @return Index of the instruction within the method code.
+	 * @return Index of the instruction within the method code <i>(As determined by ASM)</i>.
 	 */
 	public int getInstructionIndex() {
 		return index;
@@ -79,75 +75,5 @@ public class InstructionPathNode extends AbstractPathNode<ClassMember, AbstractI
 			return Integer.compare(index, node.index);
 
 		return 0;
-	}
-
-	private static String toString(AbstractInsnNode instruction) {
-		int opcode = instruction.getOpcode();
-		Textifier textifier = new Textifier();
-		switch (instruction.getType()) {
-			case AbstractInsnNode.INSN -> textifier.visitInsn(opcode);
-			case AbstractInsnNode.INT_INSN -> {
-				IntInsnNode intInsnNode = (IntInsnNode) instruction;
-				textifier.visitIntInsn(opcode, intInsnNode.operand);
-			}
-			case AbstractInsnNode.VAR_INSN -> {
-				VarInsnNode varInsnNode = (VarInsnNode) instruction;
-				textifier.visitVarInsn(opcode, varInsnNode.var);
-			}
-			case AbstractInsnNode.TYPE_INSN -> {
-				TypeInsnNode typeInsnNode = (TypeInsnNode) instruction;
-				textifier.visitTypeInsn(opcode, typeInsnNode.desc);
-			}
-			case AbstractInsnNode.FIELD_INSN -> {
-				FieldInsnNode fieldInsnNode = (FieldInsnNode) instruction;
-				textifier.visitFieldInsn(opcode, fieldInsnNode.owner, fieldInsnNode.name, fieldInsnNode.desc);
-			}
-			case AbstractInsnNode.METHOD_INSN -> {
-				MethodInsnNode methodInsnNode = (MethodInsnNode) instruction;
-				textifier.visitMethodInsn(opcode, methodInsnNode.owner, methodInsnNode.name,
-						methodInsnNode.desc, methodInsnNode.itf);
-			}
-			case AbstractInsnNode.INVOKE_DYNAMIC_INSN -> {
-				InvokeDynamicInsnNode invokeDynamicInsnNode = (InvokeDynamicInsnNode) instruction;
-				textifier.visitInvokeDynamicInsn(invokeDynamicInsnNode.name, invokeDynamicInsnNode.desc,
-						invokeDynamicInsnNode.bsm, invokeDynamicInsnNode.bsmArgs);
-			}
-			case AbstractInsnNode.JUMP_INSN -> {
-				JumpInsnNode jumpInsnNode = (JumpInsnNode) instruction;
-				textifier.visitJumpInsn(opcode, jumpInsnNode.label.getLabel());
-			}
-			case AbstractInsnNode.LABEL -> {
-				LabelNode labelNode = (LabelNode) instruction;
-				textifier.visitLabel(labelNode.getLabel());
-			}
-			case AbstractInsnNode.LDC_INSN -> {
-				LdcInsnNode ldcInsnNode = (LdcInsnNode) instruction;
-				textifier.visitLdcInsn(ldcInsnNode.cst);
-			}
-			case AbstractInsnNode.IINC_INSN -> {
-				IincInsnNode iincInsnNode = (IincInsnNode) instruction;
-				textifier.visitIincInsn(iincInsnNode.var, iincInsnNode.incr);
-			}
-			case AbstractInsnNode.TABLESWITCH_INSN -> {
-				TableSwitchInsnNode tableSwitchInsnNode = (TableSwitchInsnNode) instruction;
-				textifier.visitTableSwitchInsn(tableSwitchInsnNode.min, tableSwitchInsnNode.max,
-						tableSwitchInsnNode.dflt.getLabel(),
-						(Label[]) tableSwitchInsnNode.labels.stream().map(LabelNode::getLabel).toArray());
-			}
-			case AbstractInsnNode.LOOKUPSWITCH_INSN -> {
-				LookupSwitchInsnNode lookupSwitchInsnNode = (LookupSwitchInsnNode) instruction;
-				textifier.visitLookupSwitchInsn(lookupSwitchInsnNode.dflt.getLabel(),
-						lookupSwitchInsnNode.keys.stream().mapToInt(i -> i).toArray(),
-						(Label[]) lookupSwitchInsnNode.labels.stream().map(LabelNode::getLabel).toArray());
-			}
-			case AbstractInsnNode.MULTIANEWARRAY_INSN -> {
-				MultiANewArrayInsnNode multiANewArrayInsnNode = (MultiANewArrayInsnNode) instruction;
-				textifier.visitMultiANewArrayInsn(multiANewArrayInsnNode.desc, multiANewArrayInsnNode.dims);
-			}
-			default -> throw new UnsupportedOperationException("Unsupported instruction: " + instruction);
-		}
-		StringWriter writer = new StringWriter();
-		textifier.print(new PrintWriter(writer));
-		return writer.toString().trim();
 	}
 }
