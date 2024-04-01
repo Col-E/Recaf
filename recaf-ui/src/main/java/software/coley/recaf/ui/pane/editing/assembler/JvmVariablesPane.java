0363b781b07e9f361a14999174f471726e3f9a77
Matt <mcoley2@gmu.edu>
2024-04-01 17:56:28 -0400

    Add variable highlighting to assembler

--- recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/assembler/JvmVariablesPane.java
+++ recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/assembler/JvmVariablesPane.java
@@ -1,14 +1,18 @@
 package software.coley.recaf.ui.pane.editing.assembler;
 
+import atlantafx.base.controls.Spacer;
 import atlantafx.base.theme.Styles;
 import atlantafx.base.theme.Tweaks;
 import dev.xdark.blw.type.ClassType;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -16,23 +20,29 @@ import me.darknet.assembler.ast.ASTElement;
 import me.darknet.assembler.ast.primitive.ASTIdentifier;
 import me.darknet.assembler.ast.primitive.ASTInstruction;
 import me.darknet.assembler.ast.specific.ASTClass;
-import me.darknet.assembler.ast.specific.ASTMember;
 import me.darknet.assembler.ast.specific.ASTMethod;
 import me.darknet.assembler.compile.analysis.AnalysisResults;
 import me.darknet.assembler.compile.analysis.Local;
 import me.darknet.assembler.compile.analysis.frame.Frame;
+import me.darknet.assembler.util.Location;
 import me.darknet.assembler.util.Range;
 import org.fxmisc.richtext.CodeArea;
+import org.reactfx.Change;
 import org.reactfx.EventStreams;
 import software.coley.collections.Lists;
 import software.coley.recaf.services.cell.CellConfigurationService;
 import software.coley.recaf.ui.config.TextFormatConfig;
 import software.coley.recaf.ui.control.richtext.Editor;
+import software.coley.recaf.ui.control.richtext.linegraphics.AbstractLineGraphicFactory;
+import software.coley.recaf.ui.control.richtext.linegraphics.LineContainer;
 import software.coley.recaf.util.FxThreadUtil;
 import software.coley.recaf.util.Lang;
+import software.coley.recaf.util.SVG;
 import software.coley.recaf.workspace.model.Workspace;
 
+import java.awt.*;
 import java.time.Duration;
+import java.util.List;
 import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -47,12 +57,14 @@ import java.util.stream.Stream;
 public class JvmVariablesPane extends AstBuildConsumerComponent {
 	private final SimpleObjectProperty<Object> notifyQueue = new SimpleObjectProperty<>(new Object());
 	private final TableView<VariableData> table = new TableView<>();
+	private final Consumer<Change<Integer>> onCaretMove = this::onCaretMove;
+	private final VarHighlightLineFactory varHighlighter = new VarHighlightLineFactory();
 
 	@Inject
 	@SuppressWarnings("unchecked")
 	public JvmVariablesPane(@Nonnull CellConfigurationService cellConfigurationService,
-							@Nonnull TextFormatConfig formatConfig,
-							@Nonnull Workspace workspace) {
+	                        @Nonnull TextFormatConfig formatConfig,
+	                        @Nonnull Workspace workspace) {
 		TableColumn<VariableData, String> columnName = new TableColumn<>(Lang.get("assembler.variables.name"));
 		TableColumn<VariableData, ClassType> columnType = new TableColumn<>(Lang.get("assembler.variables.type"));
 		TableColumn<VariableData, VariableUsages> columnUsage = new TableColumn<>(Lang.get("assembler.variables.usage"));
@@ -110,6 +122,22 @@ public class JvmVariablesPane extends AstBuildConsumerComponent {
 	}
 
 	@Override
+	public void install(@Nonnull Editor editor) {
+		super.install(editor);
+
+		editor.getRootLineGraphicFactory().addLineGraphicFactory(varHighlighter);
+		editor.getCaretPosEventStream().addObserver(onCaretMove);
+	}
+
+	@Override
+	public void uninstall(@Nonnull Editor editor) {
+		super.uninstall(editor);
+
+		editor.getRootLineGraphicFactory().removeLineGraphicFactory(varHighlighter);
+		editor.getCaretPosEventStream().removeObserver(onCaretMove);
+	}
+
+	@Override
 	protected void onClassSelected() {
 		clearData();
 	}
@@ -127,6 +155,35 @@ public class JvmVariablesPane extends AstBuildConsumerComponent {
 	@Override
 	protected void onPipelineOutputUpdate() {
 		scheduleTableUpdate();
+	}
+
+	/**
+	 * Handles updating the {@link VarHighlightLineFactory}.
+	 * <p/>
+	 * This logic is shoe-horned into here <i>(for now)</i> because
+	 * the variable tracking logic is internal to this class only.
+	 *
+	 * @param caretChange
+	 * 		Caret pos change.
+	 */
+	private void onCaretMove(Change<Integer> caretChange) {
+		int pos = caretChange.getNewValue();
+
+		// Determine which variable is at the caret position
+		VariableData currentVarSelection = null;
+		for (VariableData item : table.getItems()) {
+			VariableUsages usage = item.usage();
+			ASTElement matchedAst = usage.readersAndWriters()
+					.filter(e -> e.range().within(pos))
+					.findFirst().orElse(null);
+			if (matchedAst != null) {
+				currentVarSelection = item;
+				break;
+			}
+		}
+
+		// Notify the highlighter of the difference
+		varHighlighter.setSelectedVariable(currentVarSelection);
 	}
 
 	private void scheduleTableUpdate() {
@@ -215,6 +272,78 @@ public class JvmVariablesPane extends AstBuildConsumerComponent {
 	}
 
 	/**
+	 * Highlighter which shows read and write access of a {@link VariableData}.
+	 */
+	private class VarHighlightLineFactory extends AbstractLineGraphicFactory {
+		private static final Map<RenderingHints.Key, Object> REF_RENDER_HINTS =
+				Map.of(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
+		private static final int ICON_SIZE = 16;
+		private VariableData variable;
+
+		private VarHighlightLineFactory() {
+			super(AbstractLineGraphicFactory.P_LINE_NUMBERS + 1);
+		}
+
+		/**
+		 * @param variable
+		 * 		New selected variable.
+		 */
+		public void setSelectedVariable(@Nullable VariableData variable) {
+			VariableData existing = this.variable;
+			if (existing == null) {
+				if (variable == null)
+					return;
+			} else {
+				if (existing.matchesNameType(variable))
+					return;
+			}
+
+			this.variable = variable;
+			editor.redrawParagraphGraphics();
+		}
+
+		@Override
+		public void install(@Nonnull Editor editor) {
+			// no-op, outer class has all the data we need
+		}
+
+		@Override
+		public void uninstall(@Nonnull Editor editor) {
+			// no-op
+		}
+
+		@Override
+		public void apply(@Nonnull LineContainer container, int paragraph) {
+			Node graphic;
+			createGraphic:
+			{
+				if (variable != null) {
+					for (ASTElement reader : variable.usage().readers()) {
+						Location location = reader.location();
+						if (location != null && location.line() - 1 == paragraph) {
+							graphic = SVG.ofFile(SVG.REF_READ, ICON_SIZE, REF_RENDER_HINTS);
+							graphic.prefWidth(ICON_SIZE);
+							break createGraphic;
+						}
+					}
+					for (ASTElement writer : variable.usage().writers()) {
+						Location location = writer.location();
+						if (location != null && location.line() - 1 == paragraph) {
+							graphic = SVG.ofFile(SVG.REF_WRITE, ICON_SIZE,
+									REF_RENDER_HINTS);
+							graphic.prefWidth(ICON_SIZE);
+							break createGraphic;
+						}
+					}
+				}
+				graphic = new Spacer(ICON_SIZE);
+			}
+			graphic.setCursor(Cursor.HAND);
+			container.addHorizontal(graphic);
+		}
+	}
+
+	/**
 	 * Models a variable.
 	 *
 	 * @param name
@@ -236,6 +365,17 @@ public class JvmVariablesPane extends AstBuildConsumerComponent {
 		@Nonnull
 		public static VariableData adaptFrom(@Nonnull Local local, @Nonnull VariableUsages usage) {
 			return new VariableData(local.name(), local.type(), usage);
+		}
+
+		/**
+		 * @param other
+		 * 		Other variable data to check against.
+		 *
+		 * @return {@code true} if the variable held by this data is the same as the other.
+		 */
+		public boolean matchesNameType(@Nullable VariableData other) {
+			if (other == null) return false;
+			return name.equals(other.name) && type.equals(other.type);
 		}
 	}
 
