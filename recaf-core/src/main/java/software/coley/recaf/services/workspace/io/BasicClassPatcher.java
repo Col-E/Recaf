e4d4035f61a5e85ae19050a028f91176e7eb563c
Matt <mcoley2@gmu.edu>
2025-05-30 03:27:21 -0400

    Bump CafeDude to support additional ASM crashers & improve parse performance

--- recaf-core/src/main/java/software/coley/recaf/services/workspace/io/BasicClassPatcher.java
+++ recaf-core/src/main/java/software/coley/recaf/services/workspace/io/BasicClassPatcher.java
@@ -6,10 +6,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.slf4j.Logger;
 import software.coley.cafedude.InvalidClassException;
 import software.coley.cafedude.classfile.ClassFile;
+import software.coley.cafedude.classfile.behavior.AttributeHolder;
+import software.coley.cafedude.classfile.constant.CpUtf8;
 import software.coley.cafedude.io.ClassFileReader;
 import software.coley.cafedude.io.ClassFileWriter;
 import software.coley.cafedude.transform.IllegalStrippingTransformer;
 import software.coley.recaf.analytics.logging.Logging;
+import software.coley.recaf.util.Types;
 
 import java.io.IOException;
 
@@ -30,7 +33,7 @@ public class BasicClassPatcher implements ClassPatcher {
 			ClassFileReader reader = new ClassFileReader();
 			ClassFile classFile = reader.read(code);
 			if (name == null) name = classFile.getName();
-			new IllegalStrippingTransformer(classFile).transform();
+			new IllegalStrippingTransformerExt(classFile).transform();
 			return new ClassFileWriter().write(classFile);
 		} catch (InvalidClassException ex) {
 			if (name == null) name = "<no-name-given>";
@@ -40,6 +43,25 @@ public class BasicClassPatcher implements ClassPatcher {
 			if (name == null) name = "<no-name-given>";
 			logger.error("CafeDude failed to patch '{}'", name, t);
 			throw new IOException(t);
+		}
+	}
+
+	/**
+	 * Extended illegal stripping transformer that also drops invalid signatures.
+	 */
+	private static class IllegalStrippingTransformerExt extends IllegalStrippingTransformer {
+		private IllegalStrippingTransformerExt(@Nonnull ClassFile clazz) {
+			super(clazz);
+		}
+
+		@Override
+		protected boolean matchSignature(@Nonnull CpUtf8 e, @Nonnull AttributeHolder context) {
+			return switch (context.getHolderType()) {
+				case CLASS -> Types.isValidSignature(e.getText(), Types.SignatureContext.CLASS);
+				case FIELD, RECORD_COMPONENT -> Types.isValidSignature(e.getText(), Types.SignatureContext.FIELD);
+				case METHOD -> Types.isValidSignature(e.getText(), Types.SignatureContext.METHOD);
+				case ATTRIBUTE -> false;
+			};
 		}
 	}
 }
