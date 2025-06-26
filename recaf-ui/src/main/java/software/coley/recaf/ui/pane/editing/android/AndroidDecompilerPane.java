abd224aafe46b0b3692887b6f2b9e33f8c444ef7
Matt <mcoley2@gmu.edu>
2025-06-26 01:50:26 -0400

    Add control + primary click navigation

--- recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/android/AndroidDecompilerPane.java
+++ recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/android/AndroidDecompilerPane.java
@@ -3,7 +3,11 @@ package software.coley.recaf.ui.pane.editing.android;
 import jakarta.annotation.Nonnull;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+import javafx.scene.input.MouseButton;
+import org.slf4j.Logger;
+import software.coley.recaf.analytics.logging.Logging;
 import software.coley.recaf.info.AndroidClassInfo;
+import software.coley.recaf.path.IncompletePathException;
 import software.coley.recaf.services.decompile.DecompilerManager;
 import software.coley.recaf.services.info.association.FileTypeSyntaxAssociationService;
 import software.coley.recaf.services.navigation.Actions;
@@ -24,6 +28,8 @@ import software.coley.recaf.ui.pane.editing.jvm.DecompilerPaneConfig;
  */
 @Dependent
 public class AndroidDecompilerPane extends AbstractDecompilePane {
+	private static final Logger logger = Logging.get(AndroidDecompilerPane.class);
+
 	@Inject
 	public AndroidDecompilerPane(@Nonnull DecompilerPaneConfig config,
 	                             @Nonnull KeybindingConfig keys,
@@ -48,6 +54,31 @@ public class AndroidDecompilerPane extends AbstractDecompilePane {
 				AstResolveResult result = contextActionSupport.resolvePosition(editor.getCodeArea().getCaretPosition());
 				if (result != null)
 					actions.rename(result.path());
+			} else if (keys.getGoto().match(e)) {
+				// Resolve what the caret position has, then handle navigating to the resulting path.
+				AstResolveResult result = contextActionSupport.resolvePosition(editor.getCodeArea().getCaretPosition());
+				if (result != null) {
+					try {
+						actions.gotoDeclaration(result.path());
+					} catch (IncompletePathException ex) {
+						// Should realistically never happen
+						logger.warn("Cannot goto location, path incomplete", ex);
+					}
+				}
+			}
+		});
+		setOnMouseClicked(e -> {
+			if (e.getButton() == MouseButton.PRIMARY && e.isControlDown()) {
+				// Resolve what the caret position has, then handle navigating to the resulting path.
+				AstResolveResult result = contextActionSupport.resolvePosition(editor.getCodeArea().getCaretPosition());
+				if (result != null) {
+					try {
+						actions.gotoDeclaration(result.path());
+					} catch (IncompletePathException ex) {
+						// Should realistically never happen
+						logger.warn("Cannot goto location, path incomplete", ex);
+					}
+				}
 			}
 		});
 	}
