af4a301e6c6de204962ac75f212072ffcfccf384
Matt <mcoley2@gmu.edu>
2026-04-20 22:26:45 -0400

    Cache decoded binary XML

--- recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/binary/DecodingXmlPane.java
+++ recaf-ui/src/main/java/software/coley/recaf/ui/pane/editing/binary/DecodingXmlPane.java
@@ -1,21 +1,26 @@
 package software.coley.recaf.ui.pane.editing.binary;
 
+import com.google.devrel.gmscore.tools.apk.arsc.BinaryResourceFile;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import javafx.scene.layout.BorderPane;
+import org.slf4j.Logger;
 import software.coley.android.xml.AndroidResourceProvider;
 import software.coley.android.xml.XmlDecoder;
+import software.coley.recaf.analytics.logging.Logging;
 import software.coley.recaf.info.ArscFileInfo;
 import software.coley.recaf.info.BinaryXmlFileInfo;
 import software.coley.recaf.info.FileInfo;
 import software.coley.recaf.info.TextFileInfo;
+import software.coley.recaf.info.properties.builtin.BinaryXmlDecodedProperty;
 import software.coley.recaf.path.FilePathNode;
 import software.coley.recaf.path.PathNode;
+import software.coley.recaf.services.info.association.FileTypeSyntaxAssociationService;
 import software.coley.recaf.services.navigation.FileNavigable;
 import software.coley.recaf.services.navigation.Navigable;
 import software.coley.recaf.services.navigation.UpdatableNavigable;
-import software.coley.recaf.services.info.association.FileTypeSyntaxAssociationService;
 import software.coley.recaf.ui.control.richtext.Editor;
 import software.coley.recaf.ui.control.richtext.bracket.BracketMatchGraphicFactory;
 import software.coley.recaf.ui.control.richtext.bracket.SelectedBracketTracking;
@@ -36,6 +41,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 @Dependent
 public class DecodingXmlPane extends BorderPane implements FileNavigable, UpdatableNavigable {
+	private static final Logger logger = Logging.get(DecodingXmlPane.class);
 	protected final AtomicBoolean updateLock = new AtomicBoolean();
 	protected final Editor editor;
 	protected FilePathNode path;
@@ -86,18 +92,17 @@ public class DecodingXmlPane extends BorderPane implements FileNavigable, Updata
 			if (info instanceof BinaryXmlFileInfo binaryXml) {
 				this.path = filePath;
 				try {
-					// Attempt to lookup ARSC resource for better decoding.
-					ArscFileInfo arscFile = null;
-					Workspace workspace = path.getValueOfType(Workspace.class);
-					if (workspace != null) {
-						FilePathNode arscPath = workspace.findFile(ArscFileInfo.NAME);
-						if (arscPath != null) arscFile = (ArscFileInfo) arscPath.getValue();
+					// Check if we cached the decoded XML already. If so, use it.
+					String cachedXml = BinaryXmlDecodedProperty.get(binaryXml);
+					if (cachedXml != null) {
+						FxThreadUtil.run(() -> editor.setText(cachedXml));
+						return;
 					}
 
-					// Decode XML and update the editor text.
-					AndroidResourceProvider arscResources = arscFile == null ? null : arscFile.getResourceInfo();
-					String decodedXml = XmlDecoder.decode(binaryXml.getChunkModel(), AndroidRes.getAndroidBase(), arscResources);
-					FxThreadUtil.run(() -> editor.setText(decodedXml));
+					// If not cached, decode the XML and cache it for next time.
+					Workspace workspace = path.getValueOfType(Workspace.class);
+					String decodedXml = decode(workspace, binaryXml.getChunkModel());
+					BinaryXmlDecodedProperty.set(binaryXml, decodedXml);
 				} catch (Exception ex) {
 					FxThreadUtil.run(() -> editor.setText("<!-- Failed to decode XML:\n" +
 							"Message: " + ex.getMessage() + "\n" +
@@ -105,5 +110,34 @@ public class DecodingXmlPane extends BorderPane implements FileNavigable, Updata
 				}
 			}
 		}
+	}
+
+	/**
+	 * @param workspace
+	 * 		Workspace to lookup ARSC resources from, if available. This is used to improve the quality of the decoded XML.
+	 * @param chunkModel
+	 * 		Binary XML chunk model from the {@code resources.arsc} file.
+	 *
+	 * @return Decoded XML string.
+	 */
+	@Nonnull
+	public String decode(@Nullable Workspace workspace, @Nonnull BinaryResourceFile chunkModel) {
+		// Attempt to lookup ARSC resource for better decoding.
+		ArscFileInfo arscFile = null;
+		if (workspace != null) {
+			// This assumes the ARSC file is always 'resources.arsc' in the root.
+			// This should generally always be the case, unless a developer is doing something funky.
+			FilePathNode arscPath = workspace.findFile(ArscFileInfo.NAME);
+			if (arscPath != null) arscFile = (ArscFileInfo) arscPath.getValue();
+			else {
+				logger.warn("Failed to find 'resources.arsc' in workspace, falling back to Android base model");
+			}
+		}
+
+		// Decode XML and update the editor text.
+		AndroidResourceProvider arscResources = arscFile == null ? null : arscFile.getResourceInfo();
+		String decodedXml = XmlDecoder.decode(chunkModel, AndroidRes.getAndroidBase(), arscResources);
+		FxThreadUtil.run(() -> editor.setText(decodedXml));
+		return decodedXml;
 	}
 }
