92e4c6fbd844b131425bcf51cf861aa6ee958d41
Matt <coleysoftware@outlook.com>
2026-05-11 21:47:00 -0400

    Change tests using TestBase use temp-dir config
    Should largely mitigate tests overwriting config values on dev machines

--- recaf-core/src/testFixtures/java/software/coley/recaf/test/TestBase.java
+++ recaf-core/src/testFixtures/java/software/coley/recaf/test/TestBase.java
@@ -1,14 +1,18 @@
 package software.coley.recaf.test;
 
+import jakarta.annotation.Nonnull;
 import org.jboss.weld.proxy.WeldClientProxy;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.parallel.Isolated;
 import software.coley.recaf.Bootstrap;
 import software.coley.recaf.Recaf;
+import software.coley.recaf.services.file.RecafDirectoriesConfig;
 import software.coley.recaf.services.workspace.WorkspaceManager;
 import software.coley.recaf.util.TestEnvironment;
 
 import java.lang.annotation.Annotation;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Common base for test classes using the recaf application and ensures child classes
@@ -16,10 +20,20 @@ import java.lang.annotation.Annotation;
  */
 @Isolated
 public class TestBase {
+	private static final Path TEST_BASE_DIRECTORY = createTestBaseDirectory();
 	protected static final Recaf recaf;
 	protected static WorkspaceManager workspaceManager;
 
 	static {
+		// Register marker property to indicate that we are in a test environment.
+		TestEnvironment.initTestEnv();
+
+		// Set the base directory for recaf to use during tests.
+		// We don't want to overwrite actual user data on developer machines.
+		// See: RecafDirectoriesConfig
+		System.setProperty("RECAF_DIR", TEST_BASE_DIRECTORY.toString());
+
+		// Register the test config setup bean so that we can trigger it to load and initialize the test config.
 		Bootstrap.setWeldConsumer(w -> w.addPackage(true, TestConfigSetup.class));
 		recaf = Bootstrap.get();
 
@@ -29,8 +43,6 @@ public class TestBase {
 
 	@BeforeAll
 	public static void setupWorkspaceManager() {
-		TestEnvironment.initTestEnv();
-
 		// We'll use this a lot so may as well grab it
 		workspaceManager = recaf.get(WorkspaceManager.class);
 		workspaceManager.setCurrent(null);
@@ -54,5 +66,17 @@ public class TestBase {
 			return (T) weldProxy.getMetadata().getContextualInstance();
 		}
 		return proxy;
+	}
+
+	/**
+	 * @return Test directory for {@link RecafDirectoriesConfig}.
+	 */
+	@Nonnull
+	private static Path createTestBaseDirectory() {
+		try {
+			return Files.createTempDirectory("recaf-test");
+		} catch (Exception ex) {
+			throw new IllegalStateException("Failed to create temporary Recaf test directory", ex);
+		}
 	}
 }
